ced57f3b992921aa4e69b54687b00a5adf683ec6
Vemba <vembarrajan@gmail.com>
2015-08-08 23:10:09 +0530

    Image-Slider

--- Example/js/js.js
+++ Example/js/js.js
@@ -1,5 +1,5 @@
 (function(){
-	// Set the interval to be 2 seconds
+	//Set the interval to be 2 seconds
 	var t = setInterval(function(){
 		$(".slider ul").animate({marginLeft:-200},1000,function(){
 			$(this).find("li:last").after($(this).find("li:first"));
